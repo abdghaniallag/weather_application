fa9dc12f27435cad33c7cbec7fd4a60203076496
abdghaniallag <abdghaniallag@gmail.com>
2022-02-18 16:41:18 -0800

    Resource files

---
+++ lib/presentation/onbourding/on_bourding_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OnBourdingView extends StatelessWidget {
+  const OnBourdingView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return _getWidget();
+  }
+
+  Widget _getWidget() {
+    return Container();
+  }
+}
